00484ddc8a643d6d9862d1c899461ecbe4dcbffb
ynneyavlis <yennysilvariveros@gmail.com>
2022-07-26 23:13:16 -0500

    Prueba 3

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,26 @@
 const cipher = {
-  var string="ABC"
-  string.charCodeat(0)
-};
-
+ encode: function (texto,desplaza) {
+    let respuestas = "";
+    for (let contador = 0; contador < texto.length; contador++){
+      let codigoenc = texto.charCodeAt(contador);
+      let nuevocod = ((codigoenc - 65 + desplaza) % 26) + 65;
+      let nuevoletra = String.fromCharCode(nuevocod)
+      respuestas = respuestas + nuevoletra
+    }
+    return respuestas
+   
+ }
+,
+ decode: function (texto,desplaza) {
+   let respuestas = "";
+    for (let contador = 0; contador < texto.length; contador++){
+      let codigoenc = texto.charCodeAt(contador);
+      let nuevocod = ((codigoenc - 65 + desplaza) % 26) + 65;
+      let nuevoletra = String.fromCharCode(nuevocod)
+      respuestas = respuestas + nuevoletra
+    }
+    return respuestas
+ }
+,
+}
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import cipher from './cipher.js';
+function inicio(){
+    document.getElementById("boton1").addEventListener("click",function(){
+        let texto = document.getElementById("texto").value;
+        let desplaza = parseInt(document.getElementById("desplaza").value)
+        document.getElementById("resultado").innerHTML = cipher.encode(texto,desplaza);
+    } )
+    ;
 
-console.log(cipher);
+    document.getElementById("boton2").addEventListener("click",function(){
+        let texto = document.getElementById("texto").value;
+        let desplaza = Math.abs(parseInt(document.getElementById("desplaza").value)) * -1
+        document.getElementById("resultado").innerText = cipher.decode(texto,desplaza);
+        } )
+        ;
+    }
+
+inicio();
