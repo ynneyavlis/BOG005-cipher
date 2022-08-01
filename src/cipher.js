12f33f8fe7f13519d614fbf888461262d80ee55c
ynneyavlis <yennysilvariveros@gmail.com>
2022-08-01 11:07:03 -0500

    Prueba 4

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,8 @@
 const cipher = {
- encode: function (texto,desplaza) {
+ encode: function (desplaza,texto) {
+  if(!desplaza || !texto){ 
+    throw new TypeError("Faltan Datos o invalidos")
+  }
     let respuestas = "";
     for (let contador = 0; contador < texto.length; contador++){
       let codigoenc = texto.charCodeAt(contador);
@@ -11,11 +14,14 @@ const cipher = {
    
  }
 ,
- decode: function (texto,desplaza) {
+ decode: function (desplaza,texto) {
+  if(!desplaza || !texto){ 
+    throw new TypeError("Faltan Datos o invalidos")
+  }
    let respuestas = "";
     for (let contador = 0; contador < texto.length; contador++){
       let codigoenc = texto.charCodeAt(contador);
-      let nuevocod = ((codigoenc - 65 + desplaza) % 26) + 65;
+      let nuevocod = ((codigoenc - 65 - desplaza + (26 * (desplaza % 26 + 1) )) % 26) + 65;
       let nuevoletra = String.fromCharCode(nuevocod)
       respuestas = respuestas + nuevoletra
     }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ function inicio(){
     document.getElementById("boton1").addEventListener("click",function(){
         let texto = document.getElementById("texto").value;
         let desplaza = parseInt(document.getElementById("desplaza").value)
-        document.getElementById("resultado").innerHTML = cipher.encode(texto,desplaza);
+        document.getElementById("resultado").innerHTML = cipher.encode(desplaza,texto);
     } )
     ;
 
     document.getElementById("boton2").addEventListener("click",function(){
         let texto = document.getElementById("texto").value;
-        let desplaza = Math.abs(parseInt(document.getElementById("desplaza").value)) * -1
-        document.getElementById("resultado").innerText = cipher.decode(texto,desplaza);
+        let desplaza = parseInt(document.getElementById("desplaza").value)
+        document.getElementById("resultado").innerText = cipher.decode(desplaza,texto);
         } )
         ;
     }
